f1cbdd6e3ee7691d606c64d44e51a2e7eea70486
hqkut0 <hakuto.koushiki@gmail.com>
2026-04-08 12:31:23 +0900

    base作成

---
+++ FE/src/Open/info/info.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("schedule").textContent = SCHEDULE;
+  document.getElementById("member").textContent = MEMBER_PROFILE;
+  document.getElementById("user").textContent = USER;
+});
